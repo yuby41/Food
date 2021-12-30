b1693dd7184b7cca76f7bdccaba7d2592e94fb4e
Yualbe Luis <lois21us@gmail.com>
2021-12-30 02:54:25 +0100

    Creacion del banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
             colors: {
                 primary: '#f3c614',
                 secondary: '#353535'
+            },
+
+            height: {
+                '80': '20rem',
+                '96': '24rem'
             }
         },
     },
