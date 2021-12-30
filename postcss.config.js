aca0928760836d6d69f1aa522414db045901235c
Yualbe Luis <lois21us@gmail.com>
2021-12-30 12:50:10 +0100

    Optimizacion del archivo con PurgeCSS y NanoCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+        cssnano: {
+            preset: 'default',
+        },
+    },
 }
